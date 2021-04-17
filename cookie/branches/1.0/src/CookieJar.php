d36d912ebca303bda970bee3e53ba15a3529adca
5upp0r7 <5upp0r7@23fad6b8-6d8e-48a5-9e8d-e4f1c0242fc1>
2021-04-17 11:56:56 +0000

    cookie >> 1.0.x-dev

--- cookie/branches/1.0/src/CookieJar.php
+++ cookie/branches/1.0/src/CookieJar.php
@@ -7,6 +7,7 @@ namespace Pollen\Cookie;
 use DateTimeInterface;
 use InvalidArgumentException;
 use Pollen\Support\Concerns\ConfigBagAwareTrait;
+use Pollen\Support\Exception\ManagerRuntimeException;
 use Pollen\Support\Proxy\ContainerProxy;
 use Psr\Container\ContainerInterface as Container;
 use RuntimeException;
@@ -112,7 +113,7 @@ class CookieJar implements CookieJarInterface
         if (self::$instance instanceof self) {
             return self::$instance;
         }
-        throw new RuntimeException(sprintf('Unavailable [%s] instance', __CLASS__));
+        throw new ManagerRuntimeException(sprintf('Unavailable [%s] instance', __CLASS__));
     }
 
     /**
